69838d48dd982c00af87e25a6e0e4244a8dbac1d
Laziz-Abdurasulov <lenova5192022@gmail.com>
2022-06-19 17:06:23 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,78 @@
+// ------------------1-masala--------------------------
+
+// var numbers = [1,3,0,5,0,7,0];
+// function zeroEnd(numbers){
+//     var newArray = []
+   
+//         for (num of numbers) {
+//             if(num == 0) {
+//                 newArray.push(num) 
+//             } else {
+//                 newArray.unshift(num) 
+//             }
+//         } 
+
+//         return newArray.join("");
+// }
+
+// console.log(zeroEnd(numbers));
+
+
+// ------------------------------2-masala------------------------------------------
+// var array = [false,true,false,true,false,false,true];
+
+// function countTrue(arr){
+//     var result = 0;
+//     for (num of arr) {
+//         if(num == true) {
+//             result += num;
+//         }
+        
+        
+//     }
+//     return result;
+// } 
+// console.log(countTrue(array));
+
+
+// --------------------------------------------3-masala-------------------------------------
+// var array = [1,2,0,-1,-2,3,4];
+
+
+// function numbers(arr) {
+//     var count = 0;
+// for(num of arr) {
+// if(num > 0){
+//    count += num; 
+// }
+// }
+// return count
+// } console.log(numbers(array));
+
+// ----------------------------------------------4-masala----------------------------------------
+
+
+// var obj = [ 
+//     {name:'Ali', price:200,},
+//    {name:'Ali', price:800,}, 
+//    {name:'Ali', price:400,} ,
+//  ]
+
+// function maxPrice(arr) {
+//     var max = obj[0].price;
+// 	var maxStr = '';
+    
+// for(num of arr){
+//     if(max < num.price ){
+//         var max = num.price;
+//     }
+
+//     if(max == num.price) {
+//         maxStr = num;
+//     }
+// }
+// } console.log(maxPrice(obj));
+
+
+
+
